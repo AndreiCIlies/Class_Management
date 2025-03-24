46d5a5907dcea5d659da2987f131272409065ee7
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2025-03-24 08:06:56 +0200

    KEY-32 Added files for backend authentication

---
+++ ClassManagementWebAPI/ClassManagementWebAPI/Authentication/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ClassManagementWebAPI.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ ClassManagementWebAPI/ClassManagementWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using ClassManagementWebAPI.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassManagementWebAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var result = await _authService.Register(model.Email, model.Password);
+            return Ok(new { Message = result });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            var token = await _authService.Login(model.Email, model.Password);
+            if (token == null) return Unauthorized();
+            return Ok(new { Token = token });
+        }
+    }
+}
