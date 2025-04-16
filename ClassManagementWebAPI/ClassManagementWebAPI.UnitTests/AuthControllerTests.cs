1d00a3d6d667399589b31f471b43b91707082b45
Andreea Simona <92686562+Grigoras-Andreea@users.noreply.github.com>
2025-04-16 20:39:39 +0300

    KEY-54 Removed integration tests project as it is not needed, made tests for AuthController and started tests for ClassesController

---
+++ ClassManagementWebAPI/ClassManagementWebAPI.UnitTests/AuthControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using ClassManagementWebAPI.Authentication;
+using ClassManagementWebAPI.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassManagementWebAPI.UnitTests;
+
+[TestClass]
+public class AuthControllerTests
+{
+    private IAuthService authService;
+    private AuthModel authModel = new AuthModel
+    {
+        Email = "firstname.lastname@student.com",
+        Password = "password123"
+    };
+
+    [TestInitialize]
+    public void Setup()
+    {
+        authService = Substitute.For<IAuthService>();
+    }
+
+    [TestMethod]
+    public async Task Register_ShouldReturnOk_WhenNoErrorOccurs()
+    {
+        authService.Register(Arg.Any<string>(), Arg.Any<string>())
+            .Returns(Task.FromResult("User Registered Successfully"));
+        var controller = new AuthController(authService);
+        var result = await controller.Register(authModel);
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [TestMethod]
+    public async Task Register_ShouldReturnBadRequest_WhenErrorOccurs()
+    {
+        authService.Register(Arg.Any<string>(), Arg.Any<string>())
+            .Returns(Task.FromResult("Registration Failed"));
+        var controller = new AuthController(authService);
+        var result = await controller.Register(authModel);
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+}
